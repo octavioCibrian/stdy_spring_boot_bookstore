2b99526e29e247ec981d2ececd9814388771ad9c
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-02-17 17:46:00 -0500

    Add Cache approach for our app

--- src/main/java/com/stdy/bookstore/controller/BookController.java
+++ src/main/java/com/stdy/bookstore/controller/BookController.java
@@ -4,7 +4,10 @@ package com.stdy.bookstore.controller;
 import com.stdy.bookstore.data.Book;
 import com.stdy.bookstore.repositories.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,23 +21,27 @@ public class BookController {
     private final BookRepository bookRepository;
 
     @PostMapping
+    @CacheEvict(value = "books", allEntries = true)
     public ResponseEntity<Book> saveBook(@RequestBody Book book) {
         Book savedBook = bookRepository.save(book);
         return ResponseEntity.ok(savedBook);
     }
 
     @GetMapping("/{id}")
+    @Cacheable("books")
     public ResponseEntity<Book> getBook(@PathVariable Long id) {
         Optional<Book> book = bookRepository.findById(id);
         return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping
+    @Cacheable("books")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
     @PutMapping("/{id}")
+    @CacheEvict(value = "books", allEntries = true)
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
         return bookRepository.findById(id)
                 .map(existingBook -> {
@@ -47,7 +54,9 @@ public class BookController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Transactional
     @DeleteMapping("/{id}")
+    @CacheEvict(value = "books", key = "#id")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         if (bookRepository.existsById(id)) {
             bookRepository.deleteById(id);
